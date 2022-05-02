296b3f45a6e3df8031fe0c2d2576707e6150c532
ChirimoyaJoyful <81930823+ChirimoyaJoyful@users.noreply.github.com>
2022-05-02 18:14:52 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -14,21 +14,23 @@ const pool = new Pool({
     port: 5432,
 });
 
-
 //REDIS CONNECTION
-(async () => {
-    const redisClient = redis.createClient({ url: 'redis://redis-server:6379'});
-    redisClient.on('error', (err) => {
-        console.log('An error ocurred', err);
-    });
-    redisClient.on('connect', (err) => {
-        console.log('Redis connection succesfull');
-    });
-    await redisClient.connect();
+const redisClient = redis.createClient({ url: 'redis://redis-server:6379'});
+redisClient.on('error', (err) => {
+    console.log('An error ocurred', err);
+});
+redisClient.on('connect', (err) => {
+    console.log('Redis connection succesfull');
+});
+redisClient.connect();
+
+/*(async () => {
+    
+    await 
 
     await redisClient.set('key', 'value');
     const value = await redisClient.get('key');
-})();
+})();*/
 
 
 const port = 3000;
@@ -50,6 +52,21 @@ app.get('/', async(req,res) => {
     })
 });
 
+app.get('/inventory/search', async(req, res) =>{
+    var q = req.query.q;
+    var reply = await redisClient.get(q);
+    if(reply){
+        console.log("Cache");
+    }else{
+        var items = await pool.query("SELECT * FROM items WHERE Name LIKE '%' || $1 || '%'", [q]);
+        var cosa = JSON.stringify(items.rows)
+        res.send(cosa);
+        console.log("Database");
+        await redisClient.setEx(q,600,cosa);
+    }
+});
+
+//"SELECT * FROM items WHERE Name LIKE '%' || $1 || '%'", [q]
 
 app.listen(port, () =>{
     console.log('Server running on port', port);
