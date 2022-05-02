d97735f99d7033b00661b40a12c1ef368b9cac97
ChirimoyaJoyful <81930823+ChirimoyaJoyful@users.noreply.github.com>
2022-05-01 21:36:13 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const redis = require('redis');
+const { Pool } = require('pg');
 
 
 
+//POSTGRESQL CONNECTION
+
+const pool = new Pool({
+    user: 'postgres',
+    host: 'database',
+    database: 'tiendita',
+    password: 'marihuana',
+    port: 5432,
+});
+
+
+//REDIS CONNECTION
 (async () => {
     const redisClient = redis.createClient({ url: 'redis://redis-server:6379'});
     redisClient.on('error', (err) => {
@@ -16,6 +29,8 @@ const redis = require('redis');
     await redisClient.set('key', 'value');
     const value = await redisClient.get('key');
 })();
+
+
 const port = 3000;
 
 const app = express();
@@ -23,6 +38,17 @@ const router = express.Router();
 
 app.use(express.json());
 
+
+
+app.get('/', async(req,res) => {
+    pool.query('SELECT Id FROM items', (err,res) =>{
+        if (err){
+            console.log(err.stack)
+        } else{
+            console.log(res)
+        }
+    })
+});
 
 
 app.listen(port, () =>{
